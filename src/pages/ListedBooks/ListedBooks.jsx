2fbd975b0d1c282c3d7fff2c90e1d3660c56a070
Ahamed Sumon <ahamedsumon13579@gmail.com>
2026-06-17 17:01:03 +0300

    implement listed books page

--- src/pages/ListedBooks/ListedBooks.jsx
+++ src/pages/ListedBooks/ListedBooks.jsx
@@ -1,11 +1,39 @@
-
-
+import { IoIosArrowDown } from "react-icons/io";
 const ListedBooks = () => {
-    return (
-        <div>
-            <h2>This is Listed Books page</h2>
+  return (
+    <div>
+      <h2 className="py-3 bg-[#1313130D] text-center text-3xl font-bold rounded-md">
+        Books
+      </h2>
+      <div className="text-center">
+        {/* <a className="btn bg-[#23BE0A] text-white mt-5">Sort By</a> */}
+
+        <div className="dropdown dropdown-bottom mt-5">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn m-1 bg-[#23BE0A] text-white"
+          >
+            Click{" "}
+            <span>
+              <IoIosArrowDown />
+            </span>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a>Rating</a>
+            </li>
+            <li>
+              <a>Author</a>
+            </li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ListedBooks;
